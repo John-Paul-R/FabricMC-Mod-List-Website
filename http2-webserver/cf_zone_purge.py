c3e12bd711fa3538f2ec997db20f611496749350
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2020-10-25 02:25:31 -0400

    Add cloudfare zone purge script.

---
+++ http2-webserver/cf_zone_purge.py
@@ -0,0 +1,33 @@
+
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+zones_url = "https://api.cloudflare.com/client/v4/zones"
+headers = {
+    "Authorization": "Bearer %s" % AUTH_TOKEN,
+    # "Content-Type": "application/json"
+}
+params = {
+    "name": "fibermc.com",
+    "status": "active",
+    "match": "all"
+}
+r = requests.get(
+    zones_url, 
+    headers=headers,
+    params=params
+    )
+
+resData = r.json()
+
+data = {"purge_everything": True}
+for zone in resData["result"]:
+    post = requests.post("%s/%s/purge_cache" % (zones_url, zone['id']), data=json.dumps(data), headers=headers)
+    print(post.json())
+
+
+  
